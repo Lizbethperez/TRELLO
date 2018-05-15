277eb413bc44cde9067f1292e9dcfcb30e9ccd48
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-05-14 21:59:31 -0500

    SE AGREGA LA VERSION 0.0.3 DEL PROGRAMA QUE REPLICA LA PAGINA DE TRELLO

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
 
 /*SE CREA FUNCION AÑADIR LISTA*/
-
+var section = document.getElementById('espacio-trabajo');
 var añadirLista= function(event) {
-    var section = document.getElementById('espacio-trabajo');
     newlink = document.createElement('a');
     newlink.setAttribute('class', 'formato-crear-lista'); //Se le Agrega el Atributo clase para dar Formatos desde CSS
     newlink.setAttribute('id', 'link-crear-lista');
     newlink.textContent="Añadir una Lista...";
     section.appendChild(newlink);
     newlink.addEventListener('click',formPrincipal); //SE MANDA LLAMAR LA FUNCION FORMPRINCIPAL
-}
+};
 
 //SE CREA FUNCION QUE ALMACENA EL TITULO Y ARROJA UN NUEVO BOTON O LINK
 
 var almacenarTitle = function(e){
     //console.log(e);
     e.preventDefault();
-    var section = document.getElementById('espacio-trabajo');
+    //var section = document.getElementById('espacio-trabajo');
     var valueInput=document.getElementById('input-first').value;
     var containerTitle=document.createElement('div');
     containerTitle.setAttribute('id','containerForTitle');
@@ -24,6 +23,7 @@ var almacenarTitle = function(e){
     var title=document.createElement('p');
     var textTitle=document.createTextNode(valueInput);
     title.setAttribute('class','format-title');
+    title.setAttribute('id','title')
     newLinkTarjeta=document.createElement('a');
     newLinkTarjeta.setAttribute('href','#')
     newLinkTarjeta.setAttribute('id','link-Añadir-Tarjeta');
@@ -35,32 +35,74 @@ var almacenarTitle = function(e){
     section.appendChild(containerTitle);
     var containerEliminar=document.getElementById('form');
     section.removeChild(containerEliminar);
+    newLinkTarjeta.addEventListener('click',formTextArea);
 
-}
+};
 
 /*SE CREA FUNCION PARA EL FORMULARIO PRINCIPAL CON EL BOTON DE GUARDAR*/
 var formPrincipal= function(){
-    var section = document.getElementById('espacio-trabajo');
     var container=document.createElement('div');
+    container.setAttribute('id','form')
+    container.setAttribute('class','formato-container');
     var newForm=document.createElement('form');
     var inputForm=document.createElement('input');
-    var buttonGuardar=document.createElement('button');
-    buttonGuardar.textContent="GUARDAR";
-    container.setAttribute('class','formato-container');
-    container.setAttribute('id','form')
     inputForm.setAttribute('type','text');
+    inputForm.setAttribute("id","input-first");
     inputForm.setAttribute('class','formato-input');
     inputForm.setAttribute('placeholder','Añadir una Lista....');
-    inputForm.setAttribute("id","input-first")
+    var buttonGuardar=document.createElement('button');
+    buttonGuardar.textContent="GUARDAR";
     buttonGuardar.setAttribute('class','formato-boton');
+    var buttonClose=document.createElement('button');
+    buttonClose.setAttribute('id','button-close');
+    buttonClose.setAttribute('class','format-button-close');
+    buttonClose.innerHTML="&times";
 //buttonGuardar.setAttribute('type','click');
     newForm.appendChild(inputForm);
     newForm.appendChild(buttonGuardar);
+    newForm.appendChild(buttonClose);
     container.appendChild(newForm);
     section.appendChild(container);
     var newLinkEliminar=document.getElementById('link-crear-lista');
     section.removeChild(newLinkEliminar);
     buttonGuardar.addEventListener('click',almacenarTitle);
-}
+    buttonClose.addEventListener('click', cancelForm);
+};
+
+// SE CREA LA FUNCION CON EL TEXT AREA Y EL BOTON AÑADIR
+
+var formTextArea = function(){
+    var sendTitle = document.getElementById('title');
+    var containerNewTextArea=document.createElement('div');
+    containerNewTextArea.setAttribute('class','formato-newContainer-textArea')
+    var container= document.getElementById('containerForTitle');
+    var newFormTextArea= document.createElement('form');
+    newFormTextArea.setAttribute('id','formTextArea');
+    newFormTextArea.setAttribute('class','format-form-textarea')
+    var textArea= document.createElement('textarea');
+    textArea.setAttribute('class','format-textarea');
+    //textArea.setAttribute('name','textarea');
+    textArea.setAttribute('rows','5');
+    textArea.setAttribute('cols','34');
+    var buttonAñadir=document.createElement('button');
+    buttonAñadir.setAttribute('id','buttonañadir');
+    buttonAñadir.setAttribute('class','formato-boton boton-añadir');
+    buttonAñadir.textContent="Añadir";
+    newFormTextArea.appendChild(textArea);
+    newFormTextArea.appendChild(buttonAñadir);
+    containerNewTextArea.appendChild(sendTitle);
+    containerNewTextArea.appendChild(newFormTextArea);
+    section.appendChild(containerNewTextArea);
+    var eliminarAñadirTarjeta=document.getElementById('link-Añadir-Tarjeta');
+    section.removeChild(container);
+    
+};
 añadirLista(); //SE MANDA LLAMAR A LA FUNCION
 
+//SE CREA LA FUNCION PARA ELIMINAR EL FORMULARIO AL MOMENTO DE CLICKEAR LA X 
+
+var cancelForm=function(){
+    var removeContainer=document.getElementById('form');
+    section.removeChild(removeContainer);
+    añadirLista(); //Se manda a llamar la funcion añadir lista para que esta aparezca una vez que se alla dado en la X de cerrar y se haya eliminado el formulario
+}
